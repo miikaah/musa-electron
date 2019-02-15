0a942944200c63cf15182f5d5af7d416e0a8289b
Miika Henttonen <miika.henttonen@wunderdog.fi>
2019-02-15 19:59:03 +0200

    refactor: finish the refactoring of data

--- src/library.js
+++ src/library.js
@@ -1,7 +1,14 @@
 const fs = require("fs");
 const homedir = require("os").homedir();
-const chokidar = require("chokidar");
-const { negate, startsWith, defaultTo, omit } = require("lodash");
+// const chokidar = require("chokidar");
+const {
+  negate,
+  startsWith,
+  defaultTo,
+  omit,
+  get,
+  isUndefined
+} = require("lodash");
 const { getSongMetadata } = require("./metadata");
 const { isFileTypeSupported } = require("./util");
 const Bottleneck = require("bottleneck");
@@ -11,16 +18,16 @@ const LIBRARY_PATH = `${homedir}/Documents/musat`;
 const bottleneck = new Bottleneck({ maxConcurrent: 12 });
 
 function initLibrary(mainWindow) {
-  const watcher = chokidar.watch(LIBRARY_PATH, {
-    ignored: /^\./,
-    ignoreInitial: true
-  });
-  watcher.on("add", path => {
-    console.log("add", path);
-    updateLibraryListing(mainWindow, path);
-  });
-  watcher.on("change", path => console.log("change", path));
-  watcher.on("unlink", path => console.log("unlink", path));
+  // const watcher = chokidar.watch(LIBRARY_PATH, {
+  //   ignored: /^\./,
+  //   ignoreInitial: true
+  // });
+  // watcher.on("add", path => {
+  //   console.log("add", path);
+  //   updateLibraryListing(mainWindow, path);
+  // });
+  // watcher.on("change", path => console.log("change", path));
+  // watcher.on("unlink", path => console.log("unlink", path));
 }
 
 function getLibraryListing(event) {
@@ -37,18 +44,18 @@ function getLibraryListing(event) {
   });
 }
 
-function updateLibraryListing(mainWindow, path) {
-  mainWindow.webContents.send("libraryListing", []);
-  fs.readdir(path, { withFileTypes: true }, (err, files) => {
-    if (err) {
-      console.error(err);
-      return [];
-    }
-    buildLibraryListing(path, files.filter(negate(isHiddenFile)), {
-      send: mainWindow.webContents.send
-    });
-  });
-}
+// function updateLibraryListing(mainWindow, path) {
+//   mainWindow.webContents.send("libraryListing", []);
+//   fs.readdir(path, { withFileTypes: true }, (err, files) => {
+//     if (err) {
+//       console.error(err);
+//       return [];
+//     }
+//     buildLibraryListing(path, files.filter(negate(isHiddenFile)), {
+//       send: mainWindow.webContents.send
+//     });
+//   });
+// }
 
 const isHiddenFile = file => startsWith(file.name, ".");
 
@@ -98,7 +105,29 @@ async function getAlbumsBySongs(songs) {
       metadata: await getFileMetadata(song.path)
     }))
   );
-  return reduceSongsToAlbums(songsWithMetadata);
+  const albums = reduceSongsToAlbums(songsWithMetadata);
+  const albumsAsArray = Object.keys(albums)
+    .map(name => ({
+      name,
+      songs: albums[name].songs.sort(
+        (a, b) =>
+          get(a, "metadata.track", a.name) - get(b, "metadata.track", b.name)
+      ),
+      date: mostFrequentStringInArray(
+        albums[name].songs.map(s => get(s, "metadata.date", "0"))
+      ),
+      genre: mostFrequentStringInArray(
+        albums[name].songs.map(s => get(s, "metadata.genre", ""))
+      )
+    }))
+    .map(a => {
+      // Make sure songs without an album are last in the array
+      // Atleast till this day :D
+      // Zager & Evans FTW
+      if (a.name === "undefined") return { ...a, date: "2525", genre: "" };
+      return a;
+    });
+  return albumsAsArray.sort((a, b) => a.date.localeCompare(b.date));
 }
 
 async function getFileMetadata(path) {
@@ -115,6 +144,25 @@ function reduceSongsToAlbums(songs) {
     album.songs = [...album.songs, song];
     return { ...albums, [song.metadata.album]: album };
   }, {});
+}
+
+function mostFrequentStringInArray(array) {
+  const counts = {};
+  let comparison = 0;
+  let mostFrequent;
+
+  for (let i = 0; i < array.length; i++) {
+    const word = array[i];
+
+    if (isUndefined(counts[word])) counts[word] = 1;
+    else counts[word] += 1;
+
+    if (counts[word] > comparison) {
+      comparison = counts[word];
+      mostFrequent = array[i];
+    }
+  }
+  return mostFrequent;
 }
 
 module.exports = {
