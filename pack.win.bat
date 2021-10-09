880ab54f9f654dcc6d413f079518da5d54fb7e10
Miika Henttonen <miika.henttonen@gmail.com>
2021-10-09 12:13:31 +0300

    Make build work in Windows

--- pack.win.bat
+++ pack.win.bat
@@ -1,13 +1,13 @@
 @echo off
-echo "Building Musa backend..."
+echo Building Musa backend...
 rmdir /q /s dist
 call npm run build
 
-echo "Building Musa frontend..."
+echo Building Musa frontend...
 cd %FRONTEND_DIR%
 call npm run build:electron
 
-echo "Packaging Musa into an Electron app for Windows..."
+echo Packaging Musa into an Electron app for Windows...
 cd %BACKEND_DIR%
 xcopy "%FRONTEND_DIR%\build" dist\build\ /s /e
 call npm run package:win
