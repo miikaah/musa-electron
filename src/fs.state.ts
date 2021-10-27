bc42c2a4f4f0990f063e0e0816b359b267c0631b
Miika Henttonen <miika.henttonen@wunderdog.fi>
2021-10-27 17:38:31 +0300

    Improve fs.state

--- src/fs.state.ts
+++ src/fs.state.ts
@@ -4,14 +4,34 @@ import os from "os";
 
 const { NODE_ENV } = process.env;
 const isDev = NODE_ENV === "local";
-const stateFile = `${isDev ? ".dev" : ""}.musa.state.json`;
+const stateFile = `${isDev ? ".dev" : ""}.musa-electron.state.v1.json`;
 const homedir = os.homedir();
 
-export const setState = async (state: unknown): Promise<void> => {
+type State = {
+  isInit: boolean;
+  currentTheme: {
+    id: string;
+    colors: {
+      bg: number[];
+      primary: number[];
+      secondary: number[];
+      slider: number[];
+      typography: string;
+      typographyGhost: string;
+      typographyPrimary: string;
+      typographySecondary: string;
+    };
+  };
+  replaygainType: string;
+  volume: number;
+  musicLibraryPath: string;
+};
+
+export const setState = async (state: State): Promise<void> => {
   return fs.writeFile(path.join(homedir, stateFile), JSON.stringify(state, null, 2));
 };
 
-export const getState = async (): Promise<{ musicLibraryPath: string }> => {
+export const getState = async (): Promise<Partial<State>> => {
   const file = await fs
     .readFile(path.join(homedir, stateFile), { encoding: "utf-8" })
     .catch((err) => {
