035845d770856cf87aa87615ee113c422d9ef61c
Miika Henttonen <miika.henttonen@gmail.com>
2024-08-06 18:19:40 +0300

    Use musa eslint config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,8 +1,8 @@
 // @ts-check
 import eslint from "@eslint/js";
+import eslintConfigMusa from "@miikaah/eslint-config-musa";
 import eslintConfigPrettier from "eslint-config-prettier";
 import eslintPluginPrettierRecommended from "eslint-plugin-prettier/recommended";
-import globals from "globals";
 import tseslint from "typescript-eslint";
 
 export default tseslint.config(
@@ -10,6 +10,7 @@ export default tseslint.config(
   ...tseslint.configs.recommendedTypeChecked,
   eslintConfigPrettier,
   eslintPluginPrettierRecommended,
+  ...eslintConfigMusa.getConfig(import.meta.dirname),
   {
     ignores: [
       "dist/**/*",
@@ -17,39 +18,5 @@ export default tseslint.config(
       "scripts/index.js",
       "src/preload.js",
     ],
-  },
-  {
-    languageOptions: {
-      ecmaVersion: 2022,
-      sourceType: "module",
-      globals: {
-        ...globals.node,
-      },
-      parserOptions: {
-        project: true,
-        tsconfigRootDir: import.meta.dirname,
-      },
-    },
-    rules: {
-      "prettier/prettier": 1,
-      "@typescript-eslint/no-explicit-any": "off",
-      "@typescript-eslint/no-misused-promises": "off",
-      "@typescript-eslint/no-unsafe-argument": "off",
-      "@typescript-eslint/no-unsafe-assignment": "off",
-      "@typescript-eslint/no-unsafe-call": "off",
-      "@typescript-eslint/no-unsafe-member-access": "off",
-      "@typescript-eslint/no-unsafe-return": "off",
-      "@typescript-eslint/prefer-promise-reject-errors": "off",
-      "@typescript-eslint/require-await": "off",
-      "@typescript-eslint/restrict-template-expressions": "off",
-    },
-  },
-  {
-    files: ["src/**/*.spec.ts"],
-    rules: {
-      "@typescript-eslint/no-non-null-assertion": "off",
-      "@typescript-eslint/no-explicit-any": "off",
-      "@typescript-eslint/ban-ts-comment": "off",
-    },
   },
 );
